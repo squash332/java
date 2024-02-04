7fdae1089aee95edb0f1d0068ffe817e615ebe00
hulak <95830607+squash332@users.noreply.github.com>
2024-02-04 20:53:45 +0100

    Add files via upload

---
+++ src/ponovljeni_predrok_zadatak3/Radnik.java
@@ -0,0 +1,27 @@
+package ponovljeni_predrok_zadatak3;
+
+public class Radnik extends Osoba {
+    private String ime;
+    private String prezime;
+    private double placa;
+
+    Radnik(String ime, String prezime, double placa){
+        super(ime, prezime);
+        this.placa = placa;
+    }
+    public double getPlaca(){
+        return placa;
+    }
+    public void setPlaca(double placa){
+        this.placa = placa;
+    }
+
+
+    public boolean equals(Object o){
+        if(o.equals(getPlaca())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
